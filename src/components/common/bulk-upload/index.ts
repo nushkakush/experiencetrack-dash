be65de925cff54347396bbf718078cbb6c0d6eaf
nushkakush <notfunnyin@gmail.com>
2025-08-09 20:21:27 +0530

    Fix BulkUploadDialog import error by adding default export to bulk-upload index

--- src/components/common/bulk-upload/index.ts
+++ src/components/common/bulk-upload/index.ts
@@ -1,4 +1,4 @@
-export { default as BulkUploadDialog } from './BulkUploadDialog';
+export { default, default as BulkUploadDialog } from './BulkUploadDialog';
 export type { BulkUploadConfig, ValidationResult } from './types';
 export { useBulkUpload } from './hooks/useBulkUpload';
 export { CsvParser } from './utils/csvParser';
